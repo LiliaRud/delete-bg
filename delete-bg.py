4253f2fedaa4401d925908a41a0d00d0e11932c0
Lilya Work <lilya.labunskaya@gmail.com>
2017-01-31 17:35:52 +0200

    improve code

--- delete-bg.py
+++ delete-bg.py
@@ -9,12 +9,8 @@ pix = img.load()
 
 for i in range(width):
     for j in range(height):
-        a = pix[i, j][0]
-        b = pix[i, j][1]
-        c = pix[i, j][2]
-        d = pix[i, j][3]
        
-        if a == 255 and b == 255 and c == 255 and d == 255:
+        if pix[i, j][0] == 255 and pix[i, j][1] == 255 and pix[i, j][2] == 255 and pix[i, j][3] == 255:
             pix[i, j] = (0, 0, 0, 0)
 
 img.save("images/new/new.png", "PNG")

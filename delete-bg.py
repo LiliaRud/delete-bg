4fdde2d927509c970b4c70608f7d996b60a39e7e
Lilya Work <lilya.labunskaya@gmail.com>
2017-02-01 10:52:32 +0200

    for all images in folder

--- delete-bg.py
+++ delete-bg.py
@@ -1,17 +1,27 @@
 from PIL import Image, ImageDraw
+from os import listdir
 
-Image.open("images/frog.jpg").save("images/1.png")
-image = Image.open("images/1.png")
-img = image.convert("RGBA")
-width = img.size[0]
-height = img.size[1]
-pix = img.load()
+imagesJpg = listdir("images")
 
-for i in range(width):
-    for j in range(height):
-       
-        if pix[i, j][0] == 255 and pix[i, j][1] == 255 and pix[i, j][2] == 255 and pix[i, j][3] == 255:
-            pix[i, j] = (0, 0, 0, 0)
+for a in imagesJpg:
+    if a[-3:] == "jpg":
+        Image.open("images/" + a).save("images/png/" + a + ".png")
 
-img.save("images/new/new.png", "PNG")
+imagesPng = listdir("images/png")
+
+for a in imagesPng:
+    if a[-3:] == "png":
+        image = Image.open("images/png/" + a)
+        img = image.convert("RGBA")
+        width = img.size[0]
+        height = img.size[1]
+        pix = img.load()
+
+        for i in range(width):
+            for j in range(height):
+               
+                if pix[i, j][0] == 255 and pix[i, j][1] == 255 and pix[i, j][2] == 255 and pix[i, j][3] == 255:
+                    pix[i, j] = (0, 0, 0, 0)
+
+        img.save("images/new/" + a, "PNG")
 
